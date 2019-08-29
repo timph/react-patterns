27f5df7b33fb35204848b49fab809ea5836f9a3d
Tim Pham <tim.pham@resideo.com>
2019-08-29 18:30:32 -0500

    another option for route

--- src/components/common/protectedRoute.jsx
+++ src/components/common/protectedRoute.jsx
@@ -2,30 +2,41 @@ import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { useAuth } from '../../providers/authProvider';
 
-const ProtectedRoute = ({
-  children,
-  path,
-  exact = false,
-  ...rest
-}) => {
+const ProtectedRoute = (props) => {
   const { isAuthenticated, loginWithRedirect } = useAuth();
 
   useEffect(() => {
-    (async function() {
-      if (!isAuthenticated && typeof loginWithRedirect === 'function') {
-        // @ts-ignore
-        await loginWithRedirect({
-          appState: { targetUrl: window.location.href },
-        });
-      }
-    })();
-  }, [isAuthenticated, loginWithRedirect, path]);
+    if (!isAuthenticated) {
+      loginWithRedirect({ appState: { targetUrl: window.location.href }});
+    }
+  }, [isAuthenticated, loginWithRedirect]);
 
   return (
-    <Route path={path} exact={exact} {...rest}>
-      {isAuthenticated && children}
-    </Route>
-  );
+    <Route {...props} />
+    );
 };
 
 export default ProtectedRoute;
+
+/**
+ * Reverse way of doing it
+ *
+import { Route as ReactRouterRoute, RouteProps } from 'react-router-dom';
+import useRouter from 'use-react-router';
+import { useAuth } from 'utils/authProvider';
+
+export const AnonymousRoute = ReactRouterRoute;
+
+const Route = (props: RouteProps): JSX.Element => {
+  const { auth } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated()) {
+      router.history.push('/user/sign-in');
+    }
+  }, [auth, router]);
+
+  return <ReactRouterRoute {...props} />;
+};
+ */
