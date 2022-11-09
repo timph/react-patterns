29c77d6f42194bd767c17a1d87ada88c440bb3ee
Tim Pham <timpham@paypal.com>
2022-11-09 13:28:22 -0600

    Remove antipattern isMounted

---
+++ src/components/common/ComponentLoadAbort.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from 'react';
+/**
+ * This shows a valid pattern against antipattern that checks for isMounted component to not
+ * do something.
+ */
+function ComponentLoadAbort () {
+  const [data, setData] = useState();
+
+  // use useRef so that this value does not change between rerenders
+  // not using useState because that would cause a re-render
+  const loadDataAbortControllerRef = useRef();
+
+  useEffect(() => {
+    return () => { // Abort the request during cleanup
+      if (loadDataAbortControllerRef.current) {
+        loadDataAbortControllerRef.current.abort();
+      }
+    };
+  }, []);
+
+  // This might be called anywhere, in a useEffect,
+  // in an event handler for a button click, etc
+  const loadData = () => {
+    loadDataAbortControllerRef.current = new AbortController();
+    const signal = loadDataAbortControllerRef.current.signal;
+    fetch('http://some-url/to-get-data', { signal })
+      .then(r => r.json())
+      .then(data => {
+        setData(data);
+      });
+  }
+
+  return null;
+}
